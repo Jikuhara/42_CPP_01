95335644673fe6105abc5fcad2ad9d3e49242b3e
Jikuhara <kei2003730@gmail.com>
2025-07-06 01:41:21 +0900

    EX02終わり。OK。参照って概念が増えただけ。

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,6 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kei2003730 <kei2003730@student.42.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/06 01:36:25 by kei2003730        #+#    #+#             */
+/*   Updated: 2025/07/06 01:40:08 by kei2003730       ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+int	main()
+{
+	std::string str = "HI THIS IS BRAIN";
+	std::cout << str << std::endl;
+
+	std::string* stringPTR = &str;
+	std::cout << stringPTR << std::endl;
+
+	std::string& stringREF = str;
+	std::cout << stringREF << std::endl;
+	return 0;
 }
---
+++ ex02/memo.md
@@ -0,0 +1,6 @@
+# CPP01
+## ex01
+[ゼロから学ぶCPP-ポインタと参照](https://rinatz.github.io/cpp-book/ch02-04-pointers-and-references/)
+まじめっちゃわかりやすいこのサイト。
+
+参照は、変数の別名を作るためのものってだけ。めっちゃ簡単やわ。
