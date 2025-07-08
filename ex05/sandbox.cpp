068f8dc269dc8577c2fbfe1502e3039fa8e4acaa
Jikuhara <kei2003730@gmail.com>
2025-07-08 15:13:30 +0900

    Ex05 first commit

---
+++ ex05/sandbox.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sandbox.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kei2003730 <kei2003730@student.42.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/08 12:41:55 by kei2003730        #+#    #+#             */
+/*   Updated: 2025/07/08 13:21:21 by kei2003730       ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <fstream>
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+	// if (argc != 4)
+	// {
+	// 	std::cout << "Your input is wrong." << std::endl;
+	// }
+	std::fstream s1;
+	s1.open("test.txt", std::ios_base::in | std::ios_base::out);
+	s1 << "test input" << std::endl;
+	// std::cout << s1. << std::endl;
+	/* output出来ひん:( */
+	std::fstream s2;
+	s2.open("not_exist_file_name", std::ios_base::out);
+	return 0;
+}
+
