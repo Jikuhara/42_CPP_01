ec64e0a4a1b1d44cbcace0bf7f08d27f50796a9a
Jikuhara <kei2003730@gmail.com>
2025-07-08 15:19:36 +0900

    Ex05 とりあえず関数の箱だけ作った。これ普通にIf/elseで場合分けして出力するだけ？？

--- ex05/Harl.cpp
+++ ex05/Harl.cpp
@@ -6,8 +6,33 @@
 /*   By: kei2003730 <kei2003730@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/08 15:12:36 by kei2003730        #+#    #+#             */
-/*   Updated: 2025/07/08 15:12:43 by kei2003730       ###   ########.fr       */
+/*   Updated: 2025/07/08 15:18:30 by kei2003730       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
+
+void	Harl::debug(void)
+{
+
+}
+
+void	Harl::info(void)
+{
+
+}
+
+void	Harl::warning(void)
+{
+
+}
+
+void	Harl::error(void)
+{
+
+}
+
+void	Harl::complain(std::string level)
+{
+
+}
--- ex05/Harl.hpp
+++ ex05/Harl.hpp
@@ -6,15 +6,25 @@
 /*   By: kei2003730 <kei2003730@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/08 15:12:15 by kei2003730        #+#    #+#             */
-/*   Updated: 2025/07/08 15:13:20 by kei2003730       ###   ########.fr       */
+/*   Updated: 2025/07/08 15:17:12 by kei2003730       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef HARL_HPP
 # define HARL_HPP
 
+# include <iostream>
+
 class Harl
 {
+	private:
+		void	debug(void);
+		void	info(void);
+		void	warning(void);
+		void	error(void);
+
+	public:
+		void	complain(std::string level);
 
 };
 
