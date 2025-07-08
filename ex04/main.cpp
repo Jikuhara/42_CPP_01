1ebef0cd4c46fa9de8b1973294e1b28dd87e057b
Jikuhara <kei2003730@gmail.com>
2025-07-08 15:01:32 +0900

    できたかも！！

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,22 +6,29 @@
 /*   By: kei2003730 <kei2003730@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/06 01:36:25 by kei2003730        #+#    #+#             */
-/*   Updated: 2025/07/08 14:25:15 by kei2003730       ###   ########.fr       */
+/*   Updated: 2025/07/08 15:01:04 by kei2003730       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <fstream>
 
-int replace_string(std::string str, std::string src, std::string dst)
+std::string replace_string(std::string old_str, std::string src, std::string dst)
 {
-	int search_pos = 0;
-	search_pos = str.find(src, search_pos);
-	char* ahead_chars;
-	str.copy(ahead_chars, search_pos);
-	std::string ahead_str(ahead_chars);
-	/* もうちょいきれいに実装できそう */
-	ahead_str.append(str, );
+	int old_pos = 0;
+	int new_pos = old_str.find(src, old_pos);
+	int length = old_str.length();
+	int src_length = src.length();
+	std::string new_str;
+	while (new_pos != std::string::npos)
+	{
+		new_str.append(old_str, old_pos, new_pos - old_pos);
+		new_str.append(dst);
+		old_pos = new_pos + src_length;
+		new_pos = old_str.find(src, old_pos);
+	}
+	new_str.append(old_str, old_pos, length - old_pos);
+	return (new_str);
 }
 
 int main(int argc, char const *argv[])
@@ -38,10 +45,12 @@ int main(int argc, char const *argv[])
 
 	std::string old_string;
 	std::string line;
+	std::string new_line;
 	/* 改行が一つ多くなってしまう。。 */
 	while(getline(old_file, line))
 	{
-		old_string += line + '\n';
+		new_line = replace_string(line, argv[2], argv[3]);
+		old_string += new_line + '\n';
 	}
 	new_file << old_string << std::endl;
 	return 0;
