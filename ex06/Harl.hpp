6a300abf21525908f13a29cd19f1c8eac2697634
Jikuhara <kei2003730@gmail.com>
2025-07-08 15:35:43 +0900

    Ex06 FirstCommit。や欄でも通るっぽいし後回し。

---
+++ ex06/Harl.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kei2003730 <kei2003730@student.42.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/08 15:12:15 by kei2003730        #+#    #+#             */
+/*   Updated: 2025/07/08 15:17:12 by kei2003730       ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_HPP
+# define HARL_HPP
+
+# include <iostream>
+
+class Harl
+{
+	private:
+		void	debug(void);
+		void	info(void);
+		void	warning(void);
+		void	error(void);
+
+	public:
+		void	complain(std::string level);
+
+};
+
+#endif
---
+++ ex06/main.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kei2003730 <kei2003730@student.42.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/06 01:36:25 by kei2003730        #+#    #+#             */
+/*   Updated: 2025/07/08 15:32:02 by kei2003730       ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+int main(void)
+{
+	Harl	harl;
+	harl.complain("DEBUG");
+	harl.complain("INFO");
+	harl.complain("WARNING");
+	harl.complain("ERROR");
+	harl.complain("WRONG_INPUT");
+	return 0;
+}
+
