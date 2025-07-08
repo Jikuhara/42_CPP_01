fe9354d9c6290e8d9f34a107505708501ba15213
Jikuhara <kei2003730@gmail.com>
2025-07-08 13:42:49 +0900

    コピーできた！！

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,29 +6,28 @@
 /*   By: kei2003730 <kei2003730@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/06 01:36:25 by kei2003730        #+#    #+#             */
-/*   Updated: 2025/07/06 12:14:56 by kei2003730       ###   ########.fr       */
+/*   Updated: 2025/07/08 13:42:10 by kei2003730       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "HumanA.hpp"
-#include "HumanB.hpp"
+#include <iostream>
+#include <fstream>
 
-int main()
+int main(int argc, char const *argv[])
 {
+	if (argc != 4)
 	{
-	Weapon club = Weapon("crude spiked club");
-	HumanA bob("Bob", club);
-	bob.attack();
-	club.setType("some other type of club");
-	bob.attack();
+		std::cout << "Your input is wrong." << std::endl;
+		std::cout << "Please enter ./<> <filename> <replace_string> <replace_string>." << std::endl;
 	}
-	{
-	Weapon club = Weapon("crude spiked club");
-	HumanB jim("Jim");
-	jim.setWeapon(club);
-	jim.attack();
-	club.setType("some other type of club");
-	jim.attack();
-	}
-return 0;
+	std::string old_filename = argv[1];
+	std::string new_filename = old_filename + ".replace";
+	std::fstream old_file(old_filename, std::ios_base::in);
+	std::fstream new_file(new_filename, std::ios_base::out);
+
+	std::string line;
+	getline(old_file, line);
+	new_file << line << std::endl;
+	return 0;
 }
+
